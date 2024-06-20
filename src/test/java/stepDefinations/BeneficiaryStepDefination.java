83b29c8e249a2f51e74efc7260584284dd10a188
abhijeet <abhijeet2637@gmail.com>
2024-06-21 03:58:11 +0530

    delete beneficiery api added

--- src/test/java/stepDefinations/BeneficiaryStepDefination.java
+++ src/test/java/stepDefinations/BeneficiaryStepDefination.java
@@ -10,6 +10,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class BeneficiaryStepDefination {
 
@@ -43,6 +44,7 @@ public class BeneficiaryStepDefination {
     //parsing the json
     JsonPath jS=new JsonPath(apiResponse);
 
+
     @Given("Get beneficiary api payload")
     public void getBeneficiaryApiPayload() {
         RestAssured.baseURI="https://api.tachyon.uat.hdfcbank.com";
@@ -65,13 +67,20 @@ public class BeneficiaryStepDefination {
 
     @Given("Delete beneficiary api payload")
     public void deleteBeneficiaryApiPayload() {
+        RestAssured.baseURI="https://api.tachyon.uat.hdfcbank.com";
+        reqRes=given().relaxedHTTPSValidation().log().all().header("X-Zeta-AuthToken", "eyJhbGciOiJFZERTQSJ9.eyJzdWIiOiJ1WWZ4NEl5MEM4RFlreHRhUzRPWXpRPT0iLCJ2ZXIiOiIyIiwiY2xpZW50SWQiOiJmb1dmYlFQd3l2cGRQUHVqQ0NRci5wYXl6YXAiLCJpc3MiOiIxMDAwMDAxLXBheXphcHAtdXNlcnMuaW4iLCJzYW5kYm94IjoiNCIsImFkZGl0aW9uYWxfY2xhaW1zIjp7fSwiZGV2aWNlSWQiOiJwZXJmMDAwMDAwMDE5MCIsImF1ZCI6ImNpcGhlciIsInpvbmUiOiJoYmwtYXdzLWFwczEtemV0YS1ub25wY2lkc3MtMS11YXQtZWtzIiwic3ViamVjdEpJRCI6InVZZng0SXkwQzhEWWt4dGFTNE9ZelE9PUBhdXRoUHJvZmlsZS4xMDAwMDAxLXBheXphcHAtdXNlcnMuaW4vNTkiLCJzY29wZXMiOlsibG9naW4tc2NvcGUiXSwiZXhwIjoxNzE4OTAwMDk0LCJpYXQiOjE3MTg4MTM2OTQsImp0aSI6InByb3RldXNfMTcxODgxMzY5NDMxM184ZGQ3M2I4My0yNWNkLTRjMDgtYWQxNS03MzViNjI3NTBiOGEiLCJ0ZW5hbnQiOiIxMDAwMDAxIn0.hwYQU7qcf7Dkuov1dAlPi7nyPLkKbtLQyB77HRir_OkIikVLRhduJSa9G2CIHFUlKUhErQxXRH4i0y_v87qoCQ").
+                header("Content-Type", "application/json").header("X-Request-Name", "plutus-beneficiary-service ~ Delete/Beneficiary").
+                header("Cookie", "TS01c53513=013b8e86395338d2dc7ffb462c7e80ef0d7eba9d6fa56a25bc39b19ed264aff6973939c322c7fe1517d9d039421f163679a4cefdbd").
+                queryParam("beneficiaryID", jS.getString("beneficiaryId"));
     }
 
     @When("User calls delete beneficiary api with Delete https method")
     public void userCallsDeleteBeneficiaryApiWithDeleteHttpsMethod() {
+        response=reqRes.when().delete("/gwplutus/plutus-beneficiary-service/api/v2/ifi/1000001/delete/beneficiary/uYfx4Iy0C8DYkxtaS4OYzQ==");
     }
 
     @And("status in response body is DELETED")
     public void statusInResponseBodyIsDELETED() {
+      response.then().log().all().body("status", equalTo("DELETED"));
     }
 }
